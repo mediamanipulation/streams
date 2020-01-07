aff3b9d514125657799cecb6f0ef92660991ee36
mediamanipulation <manipulation2000@hotmail.com>
2020-01-07 06:57:51 -0600

    init

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import StreamList from './stream/StreamList';
+import StreamCreate from './stream/StreamCreate';
+import StreamEdit from './stream/StreamEdit';
+import StreamDelete from './stream/StreamDelete';
+import StreamShow from './stream/StreamShow';
+
+
+
+const App = () => {
+  return (
+    <div className="ui container">
+      <BrowserRouter>
+        <Route path="/" exact component={StreamList} />
+        <Route path="/streams/new" exact component={StreamCreate} />
+        <Route path="/streams/edit" exact component={StreamEdit} />
+        <Route path="/streams/delete" exact component={StreamDelete} />
+        <Route path="/streams/show" exact component={StreamShow} />
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
