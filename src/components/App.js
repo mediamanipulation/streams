3dddba53967c29b3824dc158ed4708c771ee7126
mediamanipulation <manipulation2000@hotmail.com>
2020-01-07 21:49:04 -0600

    added header

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Link} from "react-router-dom";
+import Header from './Header'
 import StreamList from './stream/StreamList';
 import StreamCreate from './stream/StreamCreate';
 import StreamEdit from './stream/StreamEdit';
@@ -12,11 +13,14 @@ const App = () => {
   return (
     <div className="ui container">
       <BrowserRouter>
+      <Header />
+      <div>
         <Route path="/" exact component={StreamList} />
         <Route path="/streams/new" exact component={StreamCreate} />
         <Route path="/streams/edit" exact component={StreamEdit} />
         <Route path="/streams/delete" exact component={StreamDelete} />
         <Route path="/streams/show" exact component={StreamShow} />
+        </div>
       </BrowserRouter>
     </div>
   );
